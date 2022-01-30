78ede9a808e3bcbdafdf10a1a74c519b814bed32
Eisen <kanjjang89@naver.com>
2022-01-30 13:48:08 +0900

    eisenUtils is ready to launch

---
+++ eisenUtils/src/main/java/com/tistory/eisen/Eisen.java
@@ -0,0 +1,38 @@
+package com.tistory.eisen;
+
+import java.util.Arrays;
+
+public class Eisen {
+
+    public static void printArray(int[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void printArray(long[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void printArray(String[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void printArray(char[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void printArray(double[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void printArray(boolean[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void printArray(float[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void printDeepArray(Object[] o) {
+        System.out.println(Arrays.deepToString(o));
+    }
+}
---
+++ eisenUtils/src/test/java/com/tistory/eisen/EisenTest.java
@@ -0,0 +1,12 @@
+package com.tistory.eisen;
+
+import org.junit.jupiter.api.Test;
+
+
+class EisenTest {
+
+    @Test
+    void printArray() {
+
+    }
+}
