db0af9d84658d135bd8a3232df4e4ec36ba47973
Eisen <kanjjang89@naver.com>
2022-09-26 16:17:34 +0900

    Add: some configurations

--- eisenUtils/src/main/java/com/tistory/eisen/Eisen.java
+++ eisenUtils/src/main/java/com/tistory/eisen/Eisen.java
@@ -7,21 +7,21 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class Eisen {
-    private static final Logger log = LoggerFactory.getLogger(Eisen.class);
     private static final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 
     public static void printCalssName(String packagePath) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-        log.info((String) Class.forName(packagePath).newInstance());
+        System.out.println((String) Class.forName(packagePath).newInstance());
 //        ((test123)Class.forName("com.tistory.eisen.test123").newInstance()).test();  // 인스턴스화 방법.
     }
 
     public static void printArray(int[] arr) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         System.out.println(Arrays.toString(arr));
-        log.info("123");
     }
 
     public static void printArray(long[] arr) {
@@ -58,13 +58,15 @@ public class Eisen {
             func.accept(request);
         }finally {
             long elapsedTime = System.currentTimeMillis() - startTime;
-            log.debug(String.format("실행시간 => %d mes", elapsedTime));
+            System.out.println(String.format("실행시간 => %d mes", elapsedTime));
         }
     }
 
     /**
      * @Name: Annotation 기반 유효성 검증
      * @Desc: @NotNull, @NotEmpty 와 같은 Annotation 기반 유효성 검증을 수행하고 위반사항을 리턴한다.
+     * @Note: private static final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();를
+     *      static으로 주어 한번만 호출해도 되게끔. service에 들어갈때마다 굳이 생성해줄 필요가 없음.
      * @Sample:
      *      private <T> void validate(T input){
      *          Set<ConstraintViolation<T>> violations = Eisen.validate(input);
@@ -82,7 +84,7 @@ public class Eisen {
     }
 
     /**
-     * @name:  violations Set을 이용해 에러 메시지 생성
+     * @Name: violations Set을 이용해 에러 메시지 생성
      * @Sample:
      *      private <T> void validate(T input){
      *          Set<ConstraintViolation<T>> violations = Eisen.validate(input);
@@ -91,14 +93,33 @@ public class Eisen {
      *              throw new CustomException(errMsg);
      *          }
      *      }
-     * @param violations
-     * @param <T>
-     * @return
+     * @param violations Set<constraintViolation<T>
+     * @return String
      */
     public static <T> String collectViolationMessages(Set<ConstraintViolation<T>> violations){
         return  violations.stream()
             .map(e -> String.format("%s : %s", e.getPropertyPath(), e.getMessage()))
             .reduce((msg1, msg2) -> msg1 + "\n" + msg2)
             .orElse("");
+    }
+
+    /**
+     * @Name: java 9 부터 구현된 takeWile 기능
+     * @Desc:
+     *      해당 기능은 Stream의 'lazy evaluation`기능으로 filter 의 'short circuit'
+     *      이 없음을 고려하여 해당 기능을 수행 할 수 있도록 구현.
+     * @param list List<A>
+     * @param p Predicate<A>
+     * @return list List<A>
+     */
+    public static <A> List<A> takeWile(List<A> list, Predicate<A> p) {
+        int i = 0;
+        for (A item : list) {
+            if(!p.test(item)){
+                return list.subList(0, i);
+            }
+            i++;
+        }
+        return list;
     }
 }
